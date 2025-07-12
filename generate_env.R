8b115a62c10b30b0c51865ad9c9fc8b7c83507f3
mischko <mhem@mailbox.org>
2025-07-12 19:57:46 +0200

    add more packages to nix environment

--- generate_env.R
+++ generate_env.R
@@ -5,12 +5,22 @@ Sys.setenv(GITHUB_PAT = my_token)
 
 rix(
   date = "2025-07-07",
-  r_pkgs = c("languageserver", "dplyr", "Seurat"),
+  r_pkgs = c(
+    "languageserver",
+    "tidyverse",
+    "Seurat",
+    "readxl",
+    "writexl",
+    "qs2"
+  ),
   system_pkgs = NULL,
   git_pkgs = list(
     package_name = "httpgd",
     repo_url = "https://github.com/nx10/httpgd",
     commit = "dd6ed3a687a2d7327bb28ca46725a0a203eb2a19"
+    # package_name = "scMisc",
+    # repo_url = "https://github.com/mihem/scMisc",
+    # commit = "e2ebddcb779b935551f14216514c0429616fc91d"
   ),
   ide = "none",
   project_path = ".",
