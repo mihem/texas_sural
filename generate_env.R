505f17434f32bee72a4430486ceaa8fc4222f6cb
mischko <mhem@mailbox.org>
2025-07-14 22:18:51 +0200

    create automatic BPCells fix for default.nix

--- generate_env.R
+++ generate_env.R
@@ -52,3 +52,74 @@ rix(
   overwrite = TRUE,
   print = TRUE
 )
+
+# --- BPCells block fix ---
+nix_file <- "default.nix"
+nix_lines <- readLines(nix_file)
+
+bp_start <- grep("BPCells = \\(pkgs.rPackages.buildRPackage \\{", nix_lines)
+bp_end <- grep("^\\s*\\}\\);", nix_lines)
+bp_end <- bp_end[bp_end > bp_start][1]
+
+if (length(bp_start) == 1 && !is.na(bp_end)) {
+  # Extract repo URL, rev, sha256 from BPCells block
+  url_line <- grep("url = ", nix_lines[bp_start:bp_end], fixed = TRUE) +
+    bp_start -
+    1
+  rev_line <- grep("rev = ", nix_lines[bp_start:bp_end], fixed = TRUE) +
+    bp_start -
+    1
+  sha_line <- grep("sha256 = ", nix_lines[bp_start:bp_end], fixed = TRUE) +
+    bp_start -
+    1
+
+  url <- sub(".*url = \"(.*)\";.*", "\\1", nix_lines[url_line])
+  url_src <- sub("/r$", "", url) # Remove trailing /r for BPCells-src
+  rev <- sub(".*rev = \"(.*)\";.*", "\\1", nix_lines[rev_line])
+  sha <- sub(".*sha256 = \"(.*)\";.*", "\\1", nix_lines[sha_line])
+
+  # Create complete replacement block
+  replacement_block <- c(
+    "    BPCells-src = pkgs.fetchgit {",
+    sprintf("      url = \"%s\";", url_src),
+    sprintf("      rev = \"%s\";", rev),
+    sprintf("      sha256 = \"%s\";", sha),
+    "    };",
+    "",
+    "    BPCells = (pkgs.rPackages.buildRPackage {",
+    "      name = \"BPCells\";",
+    "      src = \"${BPCells-src}/r\";",
+    "      postPatch = \"patchShebangs configure\";",
+    "      nativeBuildInputs = [ pkgs.hdf5.dev ];"
+  )
+
+  propagatedBuildInputs <- grep(
+    "propagatedBuildInputs = ",
+    nix_lines[bp_start:bp_end],
+    fixed = TRUE
+  ) +
+    bp_start -
+    1
+
+  # Replace BPCells block with corrected version
+  nix_lines <- c(
+    nix_lines[1:(bp_start - 1)],
+    replacement_block,
+    nix_lines[propagatedBuildInputs:bp_end],
+    nix_lines[(bp_end + 1):length(nix_lines)]
+  )
+
+  writeLines(nix_lines, nix_file)
+}
+
+cat(
+  "###################################################################################################\n"
+)
+cat(
+  "###################################################################################################\n"
+)
+cat(
+  "###################################################################################################\n"
+)
+cat("Updated nix files successfully:\n")
+print(nix_lines)
