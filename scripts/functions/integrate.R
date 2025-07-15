d485c310d9139b52a51f2bcc4fee21cce92fceb3
mischko <mhem@mailbox.org>
2025-07-15 19:52:38 +0200

    add merge_texas_heming

--- scripts/functions/integrate.R
+++ scripts/functions/integrate.R
@@ -25,13 +25,15 @@ run_umap <- function(seu_obj) {
     )
 }
 
-# add colors to heming small misc
-add_heming_small_misc_colors <- function(seu_obj) {
+# add information to Seurat object
+add_info <- function(seu_obj) {
     heming_small_misc <- qs::qread(
         "/home/mischko/Documents/beruf/forschung/seed/sn_sural_2023_10/objects/sc_merge_small_misc.qs"
     )
     seu_obj@misc$heming_cluster_col <- heming_small_misc$cluster_col
     seu_obj@misc$heming_cluster_order <- heming_small_misc$cluster_order
+    seu_obj$level2 <- "DPN_Texas"
+    seu_obj$cluster <- seu_obj$heming_label
     seu_obj
 }
 
@@ -48,7 +50,7 @@ plot_umap <- function(seu_obj, group_by) {
         cols = seu_obj@misc$heming_cluster_col,
         label = TRUE
     ) +
-        # Seurat::NoLegend() +
+        Seurat::NoLegend() +
         ggplot2::ggtitle(group_by) +
         scMisc::theme_rect()
     ggplot2::ggsave(
@@ -61,4 +63,49 @@ plot_umap <- function(seu_obj, group_by) {
         width = 20,
         height = 8
     )
+    return(umap)
+}
+
+# Combined texas and heming Seurat objects
+merge_texas_heming <- function(seu_obj) {
+    pns_sn_heming <- qs::qread(
+        "/home/mischko/Documents/beruf/forschung/seed/sn_sural_2023_10/objects/sc_merge.qs",
+        nthreads = 4
+    )
+
+    # merge heming and texas Seurat objects
+    seu_obj_merge <- merge(
+        x = pns_sn_heming,
+        y = seu_obj,
+        merge.data = FALSE
+    )
+
+    seu_obj_merge <-
+        Seurat::DietSeurat(
+            seu_obj_merge,
+            layers = c("data"),
+            assays = "RNA",
+            dimreducs = NULL
+        )
+
+    # reduce the size of the Seurat object by creating empty data slots
+    seu_obj_merge$RNA$data.1 <-
+        Matrix::Matrix(
+            0,
+            nrow = nrow(seu_obj_merge),
+            ncol = ncol(seu_obj_merge),
+            sparse = TRUE
+        )
+
+    seu_obj_merge$RNA$data.2 <-
+        Matrix::Matrix(
+            0,
+            nrow = nrow(seu_obj_merge),
+            ncol = ncol(seu_obj_merge),
+            sparse = TRUE
+        )
+
+    seu_obj_merge@misc$heming_cluster_col <- seu_obj@misc$heming_cluster_col
+    seu_obj_merge@misc$heming_cluster_order <- seu_obj@misc$heming_cluster_order
+    return(seu_obj_merge)
 }
--- scripts/targets_integrate.R
+++ scripts/targets_integrate.R
@@ -17,7 +17,8 @@ tar_option_set(
 targets_integrate <-
     tar_plan(
         sc_merge_stacas = integrate_stacas_ss(sc_merge_predict_heming),
-        sc_merge_heming_colors = add_heming_small_misc_colors(sc_merge_stacas),
-        sc_merge_umap = run_umap(sc_merge_heming_colors),
-        umap_heming = plot_umap(sc_merge_umap, "heming_label")
+        sc_merge_info = add_info(sc_merge_stacas),
+        sc_merge_umap = run_umap(sc_merge_info),
+        umap_heming = plot_umap(sc_merge_umap, "heming_label"),
+        sc_heming_texas = merge_texas_heming(sc_merge_umap)
     )
